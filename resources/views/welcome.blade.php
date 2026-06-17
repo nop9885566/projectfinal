828cb387dcfdff7ef7d7e0df21c33941cd2f885c
Nop <aeio11za@gmail.com>
2026-06-17 13:26:18 +0700

    wel3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
 {{-- ===== LOADER ===== --}}
 <div id="loader">
   <div>
-    <div class="loader-logo">ยินดีต้อนรับ</div>
+    <div class="loader-logo">บ</div>
     <div class="loader-text">Barjong Cafe</div>
     <div class="loader-bar"><div class="loader-fill"></div></div>
   </div>
