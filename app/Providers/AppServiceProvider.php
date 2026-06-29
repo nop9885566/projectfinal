94caa25112617e5bdadbb5fa2cc95c0a0ca0c67d
Nop <aeio11za@gmail.com>
2026-06-30 01:18:58 +0700

    railway7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') !== 'local') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
         Vite::prefetch(concurrency: 3);
     }
 }
